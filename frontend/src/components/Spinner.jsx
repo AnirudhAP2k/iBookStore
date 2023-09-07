2391b110f9a03a08407733e8a920e893254612f6
AnirudhAP2k <anirudh0976@gmail.com>
2023-09-07 13:32:54 +0530

    Minor CSS fixes

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Spinner = () => {
   return (
-    <div className='animate-ping w-16 h-6 l-16 rounded-full bg-sky-600'>
+    <div className='animate-ping w-16 h-16 l-16 rounded-full bg-sky-800' style={{margin: "40px"}}>
     </div>
   )
 }
