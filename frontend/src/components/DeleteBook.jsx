bfa4c0d9e2b29dd9976548c7c2481ca6e56f54a0
AnirudhAP2k <anirudh0976@gmail.com>
2023-09-07 13:32:40 +0530

    Minor CSS fixes

--- frontend/src/components/DeleteBook.jsx
+++ frontend/src/components/DeleteBook.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
+import Backbutton from './Backbutton'
+import Spinner from './Spinner'
+import ShowBook from './ShowBook'
 
 const DeleteBook = () => {
+  
   return (
-    <div>
-      DeleteBook
-DeleteBook
+    <div className='p-4'>
+    <ShowBook />
+      div
     </div>
   )
 }
